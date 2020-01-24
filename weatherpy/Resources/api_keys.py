9133e37094488aa5ee83cfa00480e1c052cc447e
Kaleah French <kaleah.french@gmail.com>
2020-01-23 21:54:29 -0500

    JSON file reading, 1 scatterplot added

--- weatherpy/Resources/api_keys.py
+++ weatherpy/Resources/api_keys.py
@@ -1,5 +1,8 @@
 # OpenWeatherMap API Key
-weather_api_key = "fHCNMcSyfnM4RTxtu59G3Iboi1pO2kfJ3kjGdrWr"
+weather_api_key = "c4c3ace0b5186fc7174c88fa54f2747c"
+
 
 # Google API Key
 g_key = "AIzaSyAQTbyBuErgd2qo6VhFub1tJzas8cz6_uw"
+
+
